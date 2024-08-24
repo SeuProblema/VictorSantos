6540257bbaa61bb77bce953170076a8d967a9c01
Henrique Santos <henriqueisc@proton.me>
2024-08-24 12:51:29 -0300

    att

--- run.py
+++ run.py
@@ -1,48 +1,59 @@
 from flask import Flask
 from threading import Thread
-from highrise.__main__ import *
+from highrise.__main__ import main, BotDefinition
+from importlib import import_module
 import time
+import asyncio
 
+class WebServer:
+    def __init__(self):
+        self.app = Flask(__name__)
 
-class WebServer():
+        @self.app.route('/')
+        def index() -> str:
+            return "Funcionando"
 
-  def __init__(self):
-    self.app = Flask(__name__)
+    def run(self) -> None:
+        self.app.run(host='0.0.0.0', port=8081)
 
-    @self.app.route('/')
-    def index() -> str:
-      return "Funcionando"
+    def keep_alive(self):
+        t = Thread(target=self.run)
+        t.start()
 
-  def run(self) -> None:
-    self.app.run(host='0.0.0.0', port=8081)
+class RunBot:
+    room_id = "66c99406143c615afc43d3db"
+    bot_token = "7b4711b4dca23aa40167507f1faef059958bfc20f53cf9453066c520055c0dfa"
+    bot_file = "main"
+    bot_class = "Bot"
 
-  def keep_alive(self):
-    t = Thread(target=self.run)
-    t.start()
+    def __init__(self) -> None:
+        # Importa o módulo e a classe do bot dinamicamente
+        bot_module = import_module(self.bot_file)
+        bot_class = getattr(bot_module, self.bot_class)
+        
+        # Define as configurações do bot
+        self.definitions = [
+            BotDefinition(
+                bot_class(),
+                self.room_id, 
+                self.bot_token
+            )
+        ]  # Mais classes BotDefinition podem ser adicionadas à lista
 
-class RunBot():
-  room_id = "66c99406143c615afc43d3db"
-  bot_token = "7b4711b4dca23aa40167507f1faef059958bfc20f53cf9453066c520055c0dfa"
-  bot_file = "main"
-  bot_class = "Bot"
+    async def run_main(self):
+        await main(self.definitions)
 
-  def __init__(self) -> None:
-    self.definitions = [
-        BotDefinition(
-            getattr(import_module(self.bot_file), self.bot_class)(),
-            self.room_id, self.bot_token)
-    ]  # More BotDefinition classes can be added to the definitions list
-
-  def run_loop(self) -> None:
-    while True:
-      try:
-        arun(main(self.definitions))
-
-      except Exception as e:
-        print("Error: ", e)
-        time.sleep(5)
+    def run_loop(self) -> None:
+        while True:
+            try:
+                asyncio.run(self.run_main())
+            except Exception as e:
+                print("Error: ", e)
+                time.sleep(5)
 
 if __name__ == "__main__":
-  WebServer().keep_alive()
+    # Inicializa o servidor Web
+    WebServer().keep_alive()
 
-  RunBot().run_loop()
+    # Executa o loop do bot
+    RunBot().run_loop()
